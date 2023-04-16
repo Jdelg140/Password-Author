d53f4342de47938409f4223ecfc3761855b5596a
jdelg140 <jacobdelgado140@gmail.com>
2023-04-16 02:01:18 -0400

    fixed arrays and generatepassword function as well as added a promps function to display pop-ups to user

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+
+
+
+// const lowerCaseArray= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// const upperCaseArray= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+const numbersArray= [1,2,3,4,5,6,7,8,9,0];
+const specialCharactersArray= ["!","@","$","%","^","&","*","(",")","+","{","}","[","]",";",":","<",">","/","?","|","~"];
+var options= [];
+var passwordLength=[];
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-var lowerCaseArray=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upperCaseArray=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var numbersArray=["1","2","3","4","5","6","7","8","9","0"]
-var specialCharactersArray=["!","@","$","%","^","&","*","(",")","+","{","}","[","]",";",":","<",">","/","?","|","~"]
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 // Write password to the #password input
@@ -16,15 +28,45 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function prompts(){
+  options = [];
+  passwordLength =parseInt(prompt("Password will need to be between 8 and 128 characters long, choose the number of characters you would like!"));
+  
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+    alert("Password must be between 8 and 128 characters.")
+    return false;
+  }
+  
+  if (confirm("Would you like lower case letters in your password?"));{
+    options = options.concat(lowerCaseLetters);
+  }
+  if (confirm("Would you like capitalized letters in your password?"));{
+    options = options.concat(upperCaseLetters);
+  }
+  if (confirm("Would you like numbers in your password?"));{
+    options = options.concat(numbersArray);
+  }
+  if (confirm("Would you like special characters in your password?"));{
+    options = options.concat(specialCharactersArray);
+  }
+  return true;
+}
+  
+
+
 function generatePassword(){
-  var passwordLength = prompt("Password will need to be between 8 and 128 characters long, choose the number of characters you would like!")
-  var lowerCase = confirm("Would you like lower case letters in your password?")
-  var upperCase = confirm("Would you like upper case letters in your password?")
-  var numbers = confirm("Would you like numbers in your password?")
-  var specialCharacters = confirm("Would you like special characters in you password?")
+var secondPassword ="";
+for(var i=0; i <passwordLength; i++){
+ var letters = Math.floor(Math.random() * options.length);
+ secondPassword=secondPassword+ options[randomIndex];
+}
+return secondPassword;
 }
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// lowerCase = 
+//   upperCase = confirm("Would you like upper case letters in your password?");
+//   numbers = confirm("Would you like numbers in your password?");
+//   specialCharacters = confirm("Would you like special characters in you password?");
 
