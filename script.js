5bf50cd000a6692d3e3127542aaa18ee05243f26
jdelg140 <jacobdelgado140@gmail.com>
2023-04-18 12:22:11 -0400

    added semi colons to code to better organize

--- script.js
+++ script.js
@@ -1,75 +1,75 @@
 
 
-  const lowerCaseArray= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  const upperCaseArray= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  const numbersArray= [1,2,3,4,5,6,7,8,9,0];
-  const specialCharactersArray= ["!","@","$","%","^","&","*","(",")","+","{","}","[","]",";",":","<",">","/","?","|","~"];
+const lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const specialCharactersArray = ["!", "@", "$", "%", "^", "&", "*", "(", ")", "+", "{", "}", "[", "]", ";", ":", "<", ">", "/", "?", "|", "~"];
 
 
-function prompts(){
+function prompts() {
 
-  let passwordLength =parseInt(prompt("Password will need to be between 8 and 128 characters long, choose the number of characters you would like!"));
-  
-  if(Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+  let passwordLength = parseInt(prompt("Password will need to be between 8 and 128 characters long, choose the number of characters you would like!"));
+
+  if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Password must be between 8 and 128 characters.")
     return null;
   }
-  var hasLower = confirm("Would you like lower case letters in your password?")
-  var hasUpper = confirm("Would you like capitalized letters in your password?")
-  var hasNum = confirm("Would you like numbers in your password?")
-  var hasSpecial = confirm("Would you like special characters in your password?")
+  var hasLower = confirm("Would you like lower case letters in your password?");
+  var hasUpper = confirm("Would you like capitalized letters in your password?");
+  var hasNum = confirm("Would you like numbers in your password?");
+  var hasSpecial = confirm("Would you like special characters in your password?");
 
-  if (hasLower===false && hasUpper===false && hasNum===false && hasSpecial===false){
-    alert("Must select at least one character type.")
+  if (hasLower === false && hasUpper === false && hasNum === false && hasSpecial === false) {
+    alert("Must select at least one character type.");
     return null
   }
-  var passwordOptions ={
+  var passwordOptions = {
     length: passwordLength,
     hasLower: hasLower,
-    hasUpper:hasUpper,
+    hasUpper: hasUpper,
     hasNum: hasNum,
     hasSpecial: hasSpecial
   }
-return passwordOptions
- 
- }
-  
- function getRandom(arr){
-  var i= Math.floor(Math.random()*arr.length)
-  var e = arr[i]
-  return e
- }
+  return passwordOptions;
 
-function generatePassword(){
-var options = prompts()
-var result =[]
-var possibleChar = []
-var guarenteedChar = []
+}
 
-if(options.hasLower){
-  possibleChar = possibleChar.concat(lowerCaseArray)
-  guarenteedChar.push(getRandom(lowerCaseArray))
+function getRandom(arr) {
+  var i = Math.floor(Math.random() * arr.length);
+  var e = arr[i];
+  return e;
 }
-if(options.hasUpper){
-  possibleChar = possibleChar.concat(upperCaseArray)
-  guarenteedChar.push(getRandom(upperCaseArray))
-}
-if(options.hasNum){
-  possibleChar = possibleChar.concat(numbersArray)
-  guarenteedChar.push(getRandom(numbersArray))
-}
-if(options.hasSpecial){
-  possibleChar = possibleChar.concat(specialCharactersArray)
-  guarenteedChar.push(getRandom(specialCharactersArray))
-}
-for (var i=0; i<options.length; i++){
-  var char = getRandom(possibleChar)
-  result.push(char)
-}
-for (var i=0; i<guarenteedChar.length; i++){
- result[i]= guarenteedChar[i]
-}
-return result.join("")
+
+function generatePassword() {
+  var options = prompts();
+  var result = [];
+  var possibleChar = [];
+  var guarenteedChar = [];
+
+  if (options.hasLower) {
+    possibleChar = possibleChar.concat(lowerCaseArray);
+    guarenteedChar.push(getRandom(lowerCaseArray));
+  }
+  if (options.hasUpper) {
+    possibleChar = possibleChar.concat(upperCaseArray);
+    guarenteedChar.push(getRandom(upperCaseArray));
+  }
+  if (options.hasNum) {
+    possibleChar = possibleChar.concat(numbersArray);
+    guarenteedChar.push(getRandom(numbersArray));
+  }
+  if (options.hasSpecial) {
+    possibleChar = possibleChar.concat(specialCharactersArray);
+    guarenteedChar.push(getRandom(specialCharactersArray));
+  }
+  for (var i = 0; i < options.length; i++) {
+    var char = getRandom(possibleChar);
+    result.push(char);
+  }
+  for (var i = 0; i < guarenteedChar.length; i++) {
+    result[i] = guarenteedChar[i];
+  }
+  return result.join("");
 }
 
 // Assignment Code
@@ -85,7 +85,7 @@ function writePassword() {
   let password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value =password;
+  passwordText.value = password;
 }
 
 
